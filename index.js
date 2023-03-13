27393fc29bd121af31dc418bfefc6f0aa91961f7
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-03-13 12:33:18 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function  introduction(Aki){
+
+    console.log( `Hi my name is, ${Aki}`);
+
+  }
+   
+  function introductionWithLanguage(name, language){
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function introductionWithLanguageOptional(name, language="JavaScript"){
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
